1cd7ec9a426e9d1697eaa9cb0ec74b315e7aefad
nokotan <kamenokonokotan@gmail.com>
2020-04-17 02:05:51 +0900

    modify dxlib template for readme.md & build config

--- templates/hello-dxlib/build.ts
+++ templates/hello-dxlib/build.ts
@@ -9,7 +9,7 @@ gulp.task("build", async () => {
     project.getFile("include/DxDataTypeHTML5.h"),
     project.getFile("include/DxFunctionWin.h"),
     project.getFile("include/DxLib.h"),
-    ], "cpp", "wasm", "-g -O3 -lc++abi-noexcept");
+    ], "cpp", "wasm", "-Wall -O1 -s MAIN_MODULE=1 -s ALLOW_MEMORY_GROWTH=1 -s FULL_ES2=1");
   const outWasm = project.newFile("src/main.wasm", "wasm", true);
   outWasm.setData(data["a.wasm"]);
   const outJS = project.newFile("src/main.js", "javascript", true);
